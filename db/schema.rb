c3b318e9749cd14c267aef68e7b08925638f3c44
kkk1260 <n.k_02161999@ezweb.ne.jp>
2020-10-26 14:30:04 +0900

    messagesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_101456) do
+ActiveRecord::Schema.define(version: 2020_10_26_052436) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
